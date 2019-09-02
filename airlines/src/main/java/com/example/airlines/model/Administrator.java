d45de92616d70e8772d46f0ce273f1d535a593b6
Jelena <jelenas93@yahoo.com>
2019-09-02 12:31:33 +0200

    geter i seter dodani

--- airlines/src/main/java/com/example/airlines/model/Administrator.java
+++ airlines/src/main/java/com/example/airlines/model/Administrator.java
@@ -24,4 +24,45 @@ public class Administrator {
 	
 	@Column(nullable = false)
 	public boolean isActive;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswprd() {
+		return passwprd;
+	}
+
+	public void setPasswprd(String passwprd) {
+		this.passwprd = passwprd;
+	}
+
+	public AirCompany getAirCompany() {
+		return airCompany;
+	}
+
+	public void setAirCompany(AirCompany airCompany) {
+		this.airCompany = airCompany;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
 }
