001d28ec4e88ec129023cea0a138f020ff2004f8
Tamara Tintor <Tamara Tintor@DESKTOP-N7EJ21D>
2019-09-05 14:35:20 +0200

    napravila paket dao i AdministratorDAO

---
+++ airlines/src/main/java/com/example/airlines/dao/AdministratorDAO.java
@@ -0,0 +1,13 @@
+package com.example.airlines.dao;
+
+import java.util.ArrayList;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.airlines.model.Administrator;
+
+
+public interface AdministratorDAO extends CrudRepository<Server, Long> {
+
+	
+
+}
